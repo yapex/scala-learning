9c19494b7fc04a39f78ed6347e4abb2e9f34f467
YaPEX <yapex.yin@gmail.com>
2009-07-25 09:02:39 +0800

    ok

--- src/test/scala/TestByExample.scala
+++ src/test/scala/TestByExample.scala
@@ -7,6 +7,18 @@ import Assert._
  */
 class TestByExample {
 	@Test def square_roots_by_newtons_method {
-		assertTrue(true)
+		def sqrt(x: Double) = {
+			def square(x: Double) = x*x
+			def improve(guess: Double) = (guess + x/guess) /2
+			def isGoodEnough(guess: Double) = Math.abs(square(guess) - x) < 0.001
+			def sqrtIter(guess: Double): Double = 
+				if (isGoodEnough(guess)) guess
+				else sqrtIter(improve(guess))
+			sqrtIter(1.0)
+		}
+		
+		def ret = sqrt(1024)
+		println("sqrt(1024) is " + ret)
+		assertEquals(32, Math.round(ret).toInt)
 	}
 }
