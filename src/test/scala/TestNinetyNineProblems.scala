7d681fbb2b44361a9d57134bc0970db777550df7
YaPEX <yapex.yin@gmail.com>
2009-07-25 23:31:09 +0800

    start 99 problems

---
+++ src/test/scala/TestNinetyNineProblems.scala
@@ -0,0 +1,11 @@
+import org.junit._
+import Assert._
+
+/**
+ * TestNinetyNineProblems
+ */
+class TestNinetyNineProblems {
+	@Test def find_the_last_element_of_a_list_1 {
+		assertTrue(true)
+	}
+}
